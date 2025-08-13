91acc393a5beb2476ad7649f176a2f526077b39b
djumanov <djumanovdev@gmail.com>
2025-08-13 17:33:05 +0500

    echo bot with contact and photo

--- echobot.py
+++ echobot.py
@@ -2,8 +2,8 @@ from time import sleep
 import requests
 from settings import TOKEN
 
-
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}'
+
 
 def get_updates(offset: int | None):
     params = {
@@ -25,6 +25,27 @@ def send_message(chat_id: int, text: str):
     requests.get(sendmessage_url, params=params)
 
 
+def send_contact(chat_id: int, first_name: str, phone_number: str):
+    params = {
+        'chat_id': chat_id,
+        'first_name': first_name,
+        'phone_number': phone_number,
+    }
+
+    sendmessage_url = f'{BASE_URL}/sendContact'
+    requests.get(sendmessage_url, params=params)
+
+
+def send_photo(chat_id: int, photo: str):
+    params = {
+        'chat_id': chat_id,
+        'photo': photo,
+    }
+
+    sendmessage_url = f'{BASE_URL}/sendPhoto'
+    requests.get(sendmessage_url, params=params)
+
+
 def updater(token: str):
 
     offset = None
@@ -35,10 +56,19 @@ def updater(token: str):
 
         for update in updates:
             
-            if 'message' in update and 'text' in update['message']:
+            if 'message' in update:
+                message = update['message']
                 user = update['message']['from']
-                
-                send_message(user['id'], update['message']['text'])
+
+                if 'text' in message:
+                    text = message['text']
+                    send_message(user['id'], text)
+                elif 'contact' in message:
+                    contact = message['contact']
+                    send_contact(user['id'], contact['first_name'], contact['phone_number'])
+                elif 'photo' in message:
+                    photo = message['photo'][0]
+                    send_photo(user['id'], photo['file_id'])
 
             offset = update['update_id'] + 1
 
